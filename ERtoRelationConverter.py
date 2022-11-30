6ffbc28da380d0d3f20fe8af467ef32100b69a40
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-30 15:52:50 -0800

    load test

--- ERtoRelationConverter.py
+++ ERtoRelationConverter.py
@@ -1,12 +1,18 @@
 import json
 
 
-def convert():
+def convert(file):
+    er_model = json.load(file)
+    keys = er_model["key"]
+    attributes = er_model["attributes"]
+    multi_valued = er_model["multivalued"]
+    composite = er_model["composite"]
     return
 
 
 def main():
-    convert()
+    file = open("sample.json")
+    convert(file)
 
 
 if __name__ == "__main__":
