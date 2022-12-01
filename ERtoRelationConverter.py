dbfc4baa655d5e274a9897eaa837e1bf3d5d901b
nng32 <longcaster19@gmail.com>
2022-11-30 17:29:29 -0800

    Package new relationship entity into dict for pretty print

--- ERtoRelationConverter.py
+++ ERtoRelationConverter.py
@@ -4,7 +4,7 @@ underline = '\033[4m'
 
 
 def pretty_print(entity):
-    print(entity["entity"].capitalize(), end="")
+    print(entity["entity"], end="")
     print("[", end="")
     i = 0
     for key in entity["key"]:
@@ -24,7 +24,7 @@ def pretty_print(entity):
 
 
 def pick_primary(data: dict):
-    data["pk"] = list(data["key"][0])
+    data["pk"] = [data["key"][0]]
 
 
 def decompose_composite(data: dict):
@@ -33,6 +33,24 @@ def decompose_composite(data: dict):
             for attribute in attributes:
                 data["attribute"].append(attribute)
 
+
+def create_new_relation(data: dict, relation: dict) -> dict:
+    list_of_participating_entities = [relation['data'][0], relation['data'][1]]
+    first_entity = relation['data'][0].title()
+    relationship_name = relation['name'].title()
+    second_entity = relation['data'][1].title()
+    new_entity_name = first_entity + relationship_name + second_entity
+    list_of_pk = []
+
+    for entity in data:
+        if entity['entity'] in list_of_participating_entities:
+            list_of_pk.append(f"{entity['entity'].title()}{entity['pk'][0].title()}")
+
+    new_entity = {'entity': new_entity_name, 'key': list_of_pk, 'attribute': [], 'multivalued': [], 'composite': []}
+
+    return new_entity
+
+
 def main():
     with open("data.json", "r") as er_model:
         er_model = json.load(er_model)
@@ -40,8 +58,11 @@ def main():
     for entity in er_model["data"]:
         pick_primary(data=entity)
         decompose_composite(data=entity)
-        extract_multivalued_relation(data=entity, resource=er_model)
+        # extract_multivalued_relation(data=entity, resource=er_model)
         pretty_print(entity)
+
+    for relation in er_model["relation"]:
+        pretty_print(create_new_relation(er_model['data'], relation))
 
 
 if __name__ == "__main__":
