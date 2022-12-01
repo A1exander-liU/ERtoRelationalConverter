145a811a4262db4781277482a7f52d42258745af
AlexanderLiu <alexanderliu2016@gmail.com>
2022-11-30 17:10:58 -0800

    make printing function

--- ERtoRelationConverter.py
+++ ERtoRelationConverter.py
@@ -1,4 +1,26 @@
 import json
+end = '\033[0m'
+underline = '\033[4m'
+
+
+def pretty_print(entity):
+    print(entity["entity"].capitalize(), end="")
+    print("[", end="")
+    i = 0
+    for key in entity["key"]:
+        if i != len(entity["key"]) - 1:
+            print(f"{underline}{key}, {end}", end="")
+        else:
+            print(f"{underline}{key}{end}, ", end="")
+        i += 1
+    i = 0
+    for attribute in entity["attribute"]:
+        if i != len(entity["attribute"]) - 1:
+            print(f"{attribute}, ", end="")
+        else:
+            print(f"{attribute}", end="")
+        i += 1
+    print("]")
 
 
 def pick_primary(data: dict) -> str:
@@ -14,6 +36,8 @@ def decompose_composite(data: dict):
 def main():
     with open("data.json", "r") as er_model:
         er_model = json.load(er_model)
+        for entity in er_model["data"]:
+            pretty_print(entity)
 
 
 if __name__ == "__main__":
