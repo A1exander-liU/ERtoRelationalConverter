65ff4112b940f87e90c93693ccc2c897bd085521
toco <ttachibana@my.bcit.ca>
2022-11-30 16:23:32 -0800

    Define function to decompose composite attributes

--- ERtoRelationConverter.py
+++ ERtoRelationConverter.py
@@ -2,18 +2,23 @@ import json
 
 
 def pick_primary(data: dict) -> str:
-    return data["key"]
+    return data["key"][0]
 
 
 def decompose_composite(data: dict):
     for _ in data["composite"]:
-        for attribute in _.items:
-            data["attribute"].push(attribute)
+        for attributes in _.values():
+            for attribute in attributes:
+                data["attribute"].append(attribute)
 
 
 def main():
     with open("data.json", "r") as er_model:
         er_model = json.load(er_model)
+
+    for entity in er_model["data"]:
+        primary_key = pick_primary(entity)
+        decompose_composite(entity)
 
 
 if __name__ == "__main__":
